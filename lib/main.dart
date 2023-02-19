11a716d1e25461b5f7989b37d88cb9077fa83da6
Admin <makwanadharmishtha44@gmail.com>
2023-02-19 19:13:32 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:practice1/Homescreen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/':(context) => Project7(),
+      },
+    ),
+  );
+}
